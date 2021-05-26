4c5cf230131a16241e7a47ba5e327942da772520
mtygit <tim.ma0118@gmail.com>
2021-05-25 17:30:27 -0700

    updated functions in app1 and replaced app2 and app3 in index file

--- app1.js
+++ app1.js
@@ -1,5 +1,41 @@
-let userName = prompt('Please Enter Your Name');
+function getName() {
+    let userName = prompt('Please Enter Your Name');
+    if (userName == null){
+        return 'userNull';
+    } else if(userName == '') {
+        return 'userEmpty';
+    }  else {
+        return userName;
+    }
+    
+}
 
-//document.write('<h2>' + userName + '</h2>');
-// <h2>Tim</h2>
+function greeting(){
+    let user = getName();
+    if (user.toLowerCase() == 'tim'){
+        document.write('<h3>' + user + ' you have a cool name!</h3>');
+    } else{
+        document.write('<h3>Welcome ' + user + ' !</h3>');
+    }
+}
+
+
+function evLover(){
+    let carOwner = window.confirm('Do you like electric vehicles?');
+
+    if (carOwner){
+        document.write('We have an E.V. lover here!');
+    } else {
+        document.write('I wish you will find this page helpful to learn about E.V.');
+    }
+
+}
+
+
+
+
+
+
+
+
 
